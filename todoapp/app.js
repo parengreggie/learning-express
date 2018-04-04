d045c9c7ad591f3137a87da53789494bb2af5d14
reggie gokotano <reggie.gokotano@fsoft.com.vn>
2018-04-04 11:59:47 +0800

    todoapp final

--- todoapp/app.js
+++ todoapp/app.js
@@ -9,6 +9,7 @@ const port = 3000;
 const app = express();
 
 const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
 const url = 'mongodb://localhost:27017';
 const dbname = 'todoapp';
 
@@ -30,7 +31,7 @@ MongoClient.connect(url, (err, client) => {
   db = client.db(dbname);
   masterDB = client;
   // Todos = database.collection('todos');
-
+  collection = db.collection('todos');
 //  client.close();
   app.listen(port, () => {
     console.log('Server running in port: '+ port);
@@ -38,16 +39,74 @@ MongoClient.connect(url, (err, client) => {
 });
 
 app.get('/', (req, res, next) => {
-  const collection = db.collection('todos');
-
   collection.find({}).toArray((err, todos)=>{
     if(err){
       return console.log(err);
     }    
-    console.log(todos);
-    masterDB.close();
+    //console.log(todos);
+    //masterDB.close();
     res.render('index', {
       todos: todos    
     });
+  });
+});
+
+app.post('/todo/add', (req, res, next) => {
+  const todo = {
+    text: req.body.text,
+    body: req.body.body
+  }
+
+  collection.insert(todo, (err, result) => {
+    if(err) {
+      return console.log(err);
+    }
+    console.log('Todo Added...');
+    res.redirect('/');
+  });
+});
+
+app.delete('/todo/delete/:id', (req, res, next) => {
+  const query = {_id: ObjectID(req.params.id)}
+
+  collection.deleteOne(query, (err, response) => {
+    if(err) {
+      return console.log(err);
+    }
+    console.log('Todo Removed');
+    // res.send(200); // depecrated
+    res.sendStatus(200);
+  });
+});
+
+app.get('/todo/edit/:id', (req, res, next) => {
+  const query = {_id: ObjectID(req.params.id)}
+
+  collection.find(query).next((err, todo)=>{
+    if(err){
+      return console.log(err);
+    }    
+    //console.log(todos);
+    //masterDB.close();
+    res.render('edit', {
+      todo: todo
+    });
+  });
+});
+
+app.post('/todo/edit/:id', (req, res, next) => {
+  const query = {_id: ObjectID(req.params.id)}
+
+  const todo = {
+    text: req.body.text,
+    body: req.body.body
+  }
+
+  collection.updateOne(query, {$set:todo}, (err, result) => {
+    if(err) {
+      return console.log(err);
+    }
+    console.log('Todo Updated...');
+    res.redirect('/');
   });
 });
